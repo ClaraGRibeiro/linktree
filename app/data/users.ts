851f59a5f451f915d87cd6c706012eb0ee9de399
Clara Gonçalves Ribeiro <85358746+ClaraGRibeiro@users.noreply.github.com>
2026-06-23 12:23:58 -0300

    Corrigindo dados

--- app/data/users.ts
+++ app/data/users.ts
@@ -83,6 +83,7 @@ export const users = {
       },
     ],
   },
+
   guilherme_leiva: {
     nome: "Guilherme Leiva",
     foto: "/images/guilherme.jpg",
@@ -103,6 +104,18 @@ export const users = {
         url: "#",
       },
     ],
+  },
+
+  amanda_andrade: {
+    nome: "Amanda Andrade",
+    foto: "/images/amanda.jpg",
+    subtitulo: "Engenheira Civil",
+
+    lattes: "http://lattes.cnpq.br/1350365901734409",
+    instagram: "https://www.instagram.com/aevellyna/",
+    linkedin:
+      "https://www.linkedin.com/in/amanda-evellyn/",
+    whatsapp: "https://wa.me/553892403001",
   },
 
   ana_mazalotti: {
@@ -128,6 +141,18 @@ export const users = {
     whatsapp: "https://wa.me/5538992030710",
   },
 
+  erika_chaves: {
+    nome: "Érika Chaves",
+    foto: "/images/erika.jpg",
+    subtitulo: "Cientista Social",
+
+    lattes: "http://lattes.cnpq.br/0522436110177191",
+    instagram: "https://www.instagram.com/er_chaves/",
+    linkedin:
+      "https://www.linkedin.com/in/%C3%A9rikachaves2404/",
+    whatsapp: "https://wa.me/553899379259",
+  },
+
   gabriella_dias: {
     nome: "Gabriella Dias",
     foto: "/images/gabriella.jpg",
@@ -138,6 +163,18 @@ export const users = {
     linkedin:
       "https://www.linkedin.com/in/gabriella-dias-361937184/",
     whatsapp: "https://wa.me/553898623808",
+  },
+
+  julia_lopes: {
+    nome: "Julia Lopes",
+    foto: "/images/julia.jpg",
+    subtitulo: "Arquiteta + Urbanista",
+
+    lattes: "http://lattes.cnpq.br/2105599156924639",
+    instagram: "https://www.instagram.com/julialopes.arq/",
+    linkedin:
+      "#",
+    whatsapp: "https://wa.me/553892272324",
   },
 
   vanderson_silva: {
@@ -152,30 +189,6 @@ export const users = {
     whatsapp: "https://wa.me/553891612335",
   },
 
-  erika_chaves: {
-    nome: "Érika Chaves",
-    foto: "/images/erika.jpg",
-    subtitulo: "Cientista Social",
-
-    lattes: "http://lattes.cnpq.br/0522436110177191",
-    instagram: "https://www.instagram.com/er_chaves/",
-    linkedin:
-      "https://www.linkedin.com/in/%C3%A9rikachaves2404/",
-    whatsapp: "https://wa.me/553899379259",
-  },
-
-  amanda_andrade: {
-    nome: "Amanda Andrade",
-    foto: "/images/amanda.jpg",
-    subtitulo: "Engenheira Civil",
-
-    lattes: "http://lattes.cnpq.br/1350365901734409",
-    instagram: "https://www.instagram.com/aevellyna/",
-    linkedin:
-      "https://www.linkedin.com/in/amanda-evellyn/",
-    whatsapp: "https://wa.me/553892403001",
-  },
-
   yago_maia: {
     nome: "Yago Maia",
     foto: "/images/yago.jpg",
@@ -188,15 +201,4 @@ export const users = {
     whatsapp: "https://wa.me/553898557647",
   },
 
-  julia_lopes: {
-    nome: "Julia Lopes",
-    foto: "/images/julia.jpg",
-    subtitulo: "Arquiteta + Urbanista",
-
-    lattes: "http://lattes.cnpq.br/2105599156924639",
-    instagram: "https://www.instagram.com/julialopes.arq/",
-    linkedin:
-      "#",
-    whatsapp: "https://wa.me/553892272324",
-  },
 } as const;
